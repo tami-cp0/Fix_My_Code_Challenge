50546ef14747592601b1024caf8cf4854f6bb774
tami-cp0 <findtamilore>
2024-03-16 14:09:18 +0100

    updated square v3

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,21 +5,19 @@ class square():
     """Represents a square with methods to calculate its area and perimeter."""
 
     width = 0
-    height = 0
 
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, *args, **kwargs):
+        self.width = width
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def PerimeterOfMySquare(self):
         return (self.width * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}".format(self.width)
 
 
 if __name__ == "__main__":
@@ -27,4 +25,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
